116c06c04871d4ce2d4324f427be2c9d186c33fb
Noah Glaser <noahg@zoosk.com>
2020-01-18 09:03:12 -0800

    Got score to work with the pygame

--- cyclone.py
+++ cyclone.py
@@ -35,21 +35,28 @@ def create_lights_around_center(size_large_circle, size_small_circle, number_of_
     cx = WIDTH / 2
     cy = HEIGHT / 2
     points = points_on_circumference((cx, cy), size_large_circle, number_of_circles)
+    scores = []
+    for i in range(0, 10):
+        scores.append(random.randrange(10, 50))
+
+    scores.append(100)
+    random.shuffle(scores)
+
     for point in points:
-        light_sprite = Light(point[0], point[1], size_small_circle, GREEN)
+        light_sprite = Light(point[0], point[1], size_small_circle, GREEN, scores.pop(0))
         all_sprites.add(light_sprite)
         light_sprites.add(light_sprite)
         light_list.append(light_sprite)
 
+
 class Light(pygame.sprite.Sprite):
-    def __init__(self, x, y, size, color):
+    def __init__(self, x, y, size, color, points):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.size = size
         self.image = pygame.Surface([size * 2, size * 2], pygame.SRCALPHA, 32)
         self.image.convert()
-        self.image.fill(BLACK)
         self.image.set_colorkey(RED)
         self.rect = self.image.get_rect()
         # pygame.draw.circle(self.image, WHITE, (self.size, self.size), size)
@@ -61,6 +68,13 @@ class Light(pygame.sprite.Sprite):
         self.is_on = False
         self.update_light = False
         self.color = color
+        self.points = points
+        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.text = self.font.render(str(points), True, (199, 77, 227))
+        self.image.blit(self.text, [self.rect.centerx - (self.text.get_rect().width / 2), self.rect.centery - (self.rect.height / 2)])
+
+    def get_points(self):
+        return self.points
 
     def toggle_light(self, is_on):
         self.is_on = is_on
@@ -77,13 +91,13 @@ class Light(pygame.sprite.Sprite):
             gfxdraw.filled_circle(self.image, self.size, self.size, self.size, WHITE)
 
         self.update_light = False
+        self.image.blit(self.text, [self.size  - (self.text.get_rect().width / 2), self.size  - (self.text.get_rect().height / 2)])
 
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
-pygame.font.init() # you have to call this at the start,
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
+pygame.font.init()  # you have to call this at the start,
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Cyclone Game')
@@ -91,8 +105,6 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 light_sprites = pygame.sprite.Group()
 light_list = []
-
-text = myfont.render("100", True, (0, 128, 0))
 
 create_lights_around_center(200, 40, 10)
 
@@ -111,6 +123,9 @@ while running:
         # check for closing the window
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            print(light_list[next_light].get_points())
+
 
     if pygame.time.get_ticks() > next_light_timer:
         next_light_timer = pygame.time.get_ticks() + 100
@@ -129,7 +144,6 @@ while running:
     screen.fill(RED)
     all_sprites.draw(screen)
     # Change so that it uses the top of the circle
-    screen.blit(text, [light_list[3].rect.centerx, light_list[3].rect.centery])
 
     # *after* drawing everything
     pygame.display.flip()
