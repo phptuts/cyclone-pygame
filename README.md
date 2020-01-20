1f4b522a2ae3b2579817bf0521e51a2ae1fc029c
Noah Glaser <glaserpower@gmail.com>
2020-01-19 19:08:10 -0800

    Finished Game

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cyclone PyGame
+
+## Instructions
+
+Hit the space bar to get the number of points.  The goal is to get 100 points.  Press the s key to start playing.
+
+![Cyclone PyGame](/example.gif)
--- cyclone.py
+++ cyclone.py
@@ -16,10 +16,17 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+GAME_COLORS = [(31, 119, 180), (255, 127, 14),
+               (44, 160, 44), (214, 39, 40),
+               (148, 103, 189), (140, 86, 75),
+               (227, 119, 194), (127, 127, 127),
+               (188, 189, 34), (23, 190, 207)]
+
 # Assets
 
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'img')
+background_image = pygame.image.load('background.jpg')
 
 
 def points_on_circumference(center=(0, 0), r=50, n=100):
@@ -36,14 +43,13 @@ def create_lights_around_center(size_large_circle, size_small_circle, number_of_
     cy = HEIGHT / 2
     points = points_on_circumference((cx, cy), size_large_circle, number_of_circles)
     scores = []
-    for i in range(0, 10):
+    for i in range(0, number_of_circles):
         scores.append(random.randrange(10, 50))
 
     scores.append(100)
     random.shuffle(scores)
-
     for point in points:
-        light_sprite = Light(point[0], point[1], size_small_circle, GREEN, scores.pop(0))
+        light_sprite = Light(point[0], point[1], size_small_circle, random.choice(GAME_COLORS), scores.pop(0))
         all_sprites.add(light_sprite)
         light_sprites.add(light_sprite)
         light_list.append(light_sprite)
@@ -57,9 +63,7 @@ class Light(pygame.sprite.Sprite):
         self.size = size
         self.image = pygame.Surface([size * 2, size * 2], pygame.SRCALPHA, 32)
         self.image.convert()
-        self.image.set_colorkey(RED)
         self.rect = self.image.get_rect()
-        # pygame.draw.circle(self.image, WHITE, (self.size, self.size), size)
         gfxdraw.aacircle(self.image, self.size, self.size, self.size, WHITE)
         gfxdraw.filled_circle(self.image, self.size, self.size, self.size, WHITE)
         self.rect.centerx = self.x
@@ -70,11 +74,17 @@ class Light(pygame.sprite.Sprite):
         self.color = color
         self.points = points
         self.font = pygame.font.SysFont('Comic Sans MS', 30)
-        self.text = self.font.render(str(points), True, (199, 77, 227))
-        self.image.blit(self.text, [self.rect.centerx - (self.text.get_rect().width / 2), self.rect.centery - (self.rect.height / 2)])
+        self.text = self.font.render(str(points), True, BLACK)
+        self.image.blit(self.text, [self.rect.centerx - (self.text.get_rect().width / 2),
+                                    self.rect.centery - (self.rect.height / 2)])
 
     def get_points(self):
         return self.points
+
+    def set_points(self, new_points):
+        self.points = new_points
+        self.update_light = True
+        self.is_on = False
 
     def toggle_light(self, is_on):
         self.is_on = is_on
@@ -89,9 +99,11 @@ class Light(pygame.sprite.Sprite):
             self.is_on = True
             gfxdraw.aacircle(self.image, self.size, self.size, self.size, WHITE)
             gfxdraw.filled_circle(self.image, self.size, self.size, self.size, WHITE)
-
         self.update_light = False
-        self.image.blit(self.text, [self.size  - (self.text.get_rect().width / 2), self.size  - (self.text.get_rect().height / 2)])
+        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.text = self.font.render(str(self.points), True, BLACK)
+        self.image.blit(self.text,
+                        [self.size - (self.text.get_rect().width / 2), self.size - (self.text.get_rect().height / 2)])
 
 
 # initialize pygame and create window
@@ -106,13 +118,16 @@ all_sprites = pygame.sprite.Group()
 light_sprites = pygame.sprite.Group()
 light_list = []
 
-create_lights_around_center(200, 40, 10)
+number_of_circles = 25
+create_lights_around_center(240, 20, number_of_circles)
 
 running = True
 
 next_light_timer = pygame.time.get_ticks() + 100
 next_light = 0
 light_list[next_light].toggle_light(True)
+is_playing = False
+points = 0
 
 while running:
 
@@ -124,10 +139,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            print(light_list[next_light].get_points())
+            points = light_list[next_light].get_points()
+            random_points = []
+            for i in range(0, number_of_circles):
+                random_points.append(random.randrange(10, 50))
 
+            random_points.append(100)
+            random.shuffle(random_points)
+            print(random_points)
+            for i in range(0, number_of_circles + 1):
+                light_list[i].set_points(random_points.pop(0))
 
-    if pygame.time.get_ticks() > next_light_timer:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_s and not is_playing:
+            is_playing = True
+
+    if pygame.time.get_ticks() > next_light_timer and is_playing:
         next_light_timer = pygame.time.get_ticks() + 100
         light_list[next_light].toggle_light(False)
         if next_light >= len(light_list) - 1:
@@ -141,8 +167,28 @@ while running:
     all_sprites.update()
 
     # Draw / Render
-    screen.fill(RED)
+    screen.blit(background_image, [0, 0])
     all_sprites.draw(screen)
+    if not is_playing:
+        game_text_top = pygame.font.SysFont('Comic Sans MS', 40).render("Press s to start", True, WHITE)
+        game_text_top_x = (WIDTH / 2) - game_text_top.get_rect().width / 2
+        game_text_top_y = 250
+
+        game_text_bottom = pygame.font.SysFont('Comic Sans MS', 40).render("Press the space bar to play.", True, WHITE)
+        game_text_bottom_x = (WIDTH / 2) - game_text_bottom.get_rect().width / 2
+        game_text_bottom_y = 300
+
+        screen.blit(game_text_bottom, [game_text_bottom_x, game_text_bottom_y])
+
+        screen.blit(game_text_top, [game_text_top_x, game_text_top_y])
+
+    if is_playing and points > 0:
+        game_text_top = pygame.font.SysFont('Comic Sans MS', 40).render("Points: " + str(points), True, WHITE)
+        game_text_top_x = (WIDTH / 2) - game_text_top.get_rect().width / 2
+        game_text_top_y = 250
+
+        screen.blit(game_text_top, [game_text_top_x, game_text_top_y])
+
     # Change so that it uses the top of the circle
 
     # *after* drawing everything
